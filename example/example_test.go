958f8d51fe9099d3c0533bd6bc08eecae8a0da2a
Yu-Huai-M <1346730782@qq.com>
2025-05-24 10:49:21 +0800

    测试功能

---
+++ example/example_test.go
@@ -0,0 +1,21 @@
+package example
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	result := add(1, 2)
+	if result != 3 {
+		t.Errorf("add(1, 2)=%d; want %d", result, 3)
+	}
+	t.Logf("test add success")
+}
+
+func TestSub(t *testing.T) {
+	result := sub(1, 2)
+	if result != -1 {
+		t.Errorf("sub(1, 2)=%d; want %d", result, -1)
+	}
+	t.Logf("test sub success")
+}
